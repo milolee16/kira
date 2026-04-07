71abe105769e5b5539773227283c1a8244d942b2
kkmin20200331-max <kkmin20200331@gmil.com>
2026-04-07 09:40:16 +0900

    [add] tk , R function complete

--- src/main/java/com/kira/pj/photo/PhotoC.java
+++ src/main/java/com/kira/pj/photo/PhotoC.java
@@ -13,22 +13,19 @@ public class PhotoC extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
+        response.getWriter().println(PhotoDAO.PDAO.getJson());
 
-        // DB에서 가져온 데이터라고 가정 (객체 리스트)
+    }
 
-//        String jsonResponse = "[" +
-//                "{\"userId\":\"user1\", \"imgName\":\"pic1.jpg\", \"title\":\"남산타워에서\"}," +
-//                "{\"userId\":\"user2\", \"imgName\":\"pic2.jpg\", \"title\":\"코딩 중 졸림\"}" +
-//                "]";
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        String jsonResponse = "[" +
-                "{\"userId\":\"user1\", \"imgName\":\"pic1.jpg\", \"title\":\"남산타워에서\", \"regDate\":\"2024.03.25\", \"content\":\"날씨가 너무 좋아서 남산타워 다녀옴! 야경이 예술이다 진짜...\"}," +
-                "{\"userId\":\"user2\", \"imgName\":\"pic2.jpg\", \"title\":\"코딩 중\", \"regDate\":\"2024.03.26\", \"content\":\"세미 프로젝트 마감 직전. 죽겠다... 살려줘... 💻☕\"}," +
-                "{\"userId\":\"user1\", \"imgName\":\"pic3.jpg\", \"title\":\"맛있는 점심\", \"regDate\":\"2024.03.27\", \"content\":\"오늘 점심은 돈까스! 찍먹 vs 부먹 당신의 선택은?\"}" +
-                "]";
+    }
 
-        response.getWriter().write(jsonResponse);
-//        request.getRequestDispatcher("photo/photo.jsp").forward(request, response);
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
     }
 
--- src/main/java/com/kira/pj/photo/PhotoDAO.java
+++ src/main/java/com/kira/pj/photo/PhotoDAO.java
@@ -2,23 +2,43 @@ package com.kira.pj.photo;
 
 import com.kira.pj.main.DBManager;
 
-import javax.servlet.http.HttpServletRequest;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class PhotoDAO {
-    public static void test(HttpServletRequest request) {
-        try {
-            Connection con = DBManager.connect();
-            PreparedStatement ps = con.prepareStatement("select * from pictures");
-            ResultSet rs = ps.executeQuery();
-            rs.next();
-            System.out.println(rs.getString("p_text"));
-        } catch (Exception e) {
-e.printStackTrace();
-        }
+    public static final PhotoDAO PDAO = new PhotoDAO();
 
+    public ArrayList<String> getJson() {
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "select * from photo";
+        try {
+           conn = DBManager.connect();
+           ps = conn.prepareStatement(sql);
+           rs = ps.executeQuery();
+           ArrayList<String> photos = new ArrayList<>();
+           PhotoDTO photo = new  PhotoDTO();
+           while (rs.next()) {
+               photo.setUserId(rs.getString(2));
+               photo.setImgName(rs.getString(3));
+               photo.setTitle(rs.getString(4));
+               photo.setContent(rs.getString(5));
+               photo.setRegDate(rs.getString(6));
+               photos.add(photo.toJSON());
+
+           }
+            System.out.println(photos);
+            return photos;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            DBManager.close(conn, ps, rs);
+        }
+        return null;
     }
 }
